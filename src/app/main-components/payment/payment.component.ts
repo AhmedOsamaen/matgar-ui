9123ed80f183eb9a077eca2661e7acd43e0ab089
Ahmed_Saleh0036 <ahmedsaleh0036@gmail.com>
2022-11-03 17:54:57 +0200

    update payment popup window

--- src/app/main-components/payment/payment.component.ts
+++ src/app/main-components/payment/payment.component.ts
@@ -1,5 +1,14 @@
+import { Inject} from '@angular/core';
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
 import { Component, OnInit } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AddPaymentComponent } from '../add-payment/add-payment.component';
 
 @Component({
   selector: 'app-payment',
@@ -8,9 +17,24 @@ import { Router } from '@angular/router';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  name=""
+  animal=""
+  openDialog(): void {
+    
+    const dialogRef = this.dialog.open(AddPaymentComponent, {
+      width: '35%',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
   }
 
   addPaymentCard():void{
