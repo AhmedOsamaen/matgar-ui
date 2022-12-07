f30ee45d451237da9bbbeb11f3e4115ba0c7c78e
Ahmed_Saleh0036 <ahmedsaleh0036@gmail.com>
2022-12-07 19:25:23 +0200

    convert order_id from static to dynamic with Stage (CArt)

--- src/app/main-components/navbar/navbar.component.ts
+++ src/app/main-components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Order } from './../../Models/Order';
 import { ProductsService } from './../../Services/products.service';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/Services/user.service';
+import { Stage } from 'src/app/Modules/stage';
 
 @Component({
   selector: 'app-navbar',
@@ -9,10 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   cartItemsCount:number=0;
-  constructor(private productService:ProductsService) { }
+  constructor(private productService:ProductsService,private userService:UserService) { }
+  order: any;
 
   ngOnInit(): void {
-    this.getCartCount()
+    
     this.productService.cartItems.subscribe(result=>{
       if(result=='add'){
         this.cartItemsCount++;
@@ -21,10 +24,20 @@ export class NavbarComponent implements OnInit {
       }
     })
   }
+
+  getUserOrder_Cart(){
+    
+    this.userService.getUserOrdersStage(13,Stage.CART).subscribe(response=>{
+      this.order = response
+      this.getCartCount();
+    })
+  }
+
   getCartCount(){
-    const req:Order = {id:24}
-    this.productService.getCartProducts(req).subscribe(response=>{
+    this.productService.getCartProducts(this.order).subscribe(response=>{
       this.cartItemsCount=response;
     })
+    // const req:Order = {id:24}
+    
   }
 }
